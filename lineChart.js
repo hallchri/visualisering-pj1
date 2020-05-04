8e7c32f903bca1d999c84bdb5cf170149a57b3a5
hallchri <hallchri@arcada.fi>
2020-05-04 13:02:40 +0300

    Skapade en generator
    d3.line är en path d = stränggenerator
    med många praktiska metoder

--- lineChart.js
+++ lineChart.js
@@ -1,4 +1,8 @@
 function drawChart() {
+
+    // En array med våra x och y koordinater för cassiopeia
+    var dataArray = [ {x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10} ];
+
     var width = 500;
     var height = 300;
 
@@ -8,10 +12,16 @@ function drawChart() {
         .attr('width', width)
         .attr('height', height);
 
-    // rita en linje (obs path inte svg line)
-    // <path id="lineAB" d="M 100 350 l 150 -300" stroke="red" fill="none" />
-    canvas.append('path')
+        // d3.line() är en generator som genererar en sträng för d="M x y ...
+    var path = d3.line()
+        .x( function(data) { return data.x * 6 } )
+        .y( function(data) { return data.y * 6 } )
+        .curve(d3.curveCardinal);
+    // DEBUG: console.log(path(dataArray));
+    
+    
+    canvas.append('path') // rita en linje (obs path inte svg line)
         .attr('fill','none')
         .attr('stroke','blue')
-        .attr('d', "M30 20 L60 60 L120 28 L180 72 L240 40")
+        .attr('d', path(dataArray)); // <path id="lineAB" d="M 100 350 l 150 -300" stroke="red" fill="none" />
 }
