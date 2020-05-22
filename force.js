38298f076538b0511b123aac906537a0f015d40e
hallchri <hallchri@arcada.fi>
2020-05-22 13:14:41 +0300

    Update force.js
    lagt till labels, centreringskraft, mouseout/in funktioner m.m

--- force.js
+++ force.js
@@ -3,6 +3,8 @@ function drawForce() {
 
         var width = 400, height = 400;
 
+        var  gravityCenterX = width/2, gravityCenterY = height/2;
+
         var canvas = d3.select('body').append('svg')
             .attr('width', width)
             .attr('height', height);
@@ -14,10 +16,50 @@ function drawForce() {
             .enter().append('ellipse')
             .attr('cx', 100)
             .attr('cy', 100)
-            .attr('rx', 20)
-            .attr('ry', 15)
+            .attr('rx', 40)
+            .attr('ry', 20)
             .attr('stroke', 'black')
-            .attr('fill', 'lightgrey');
+            .attr('fill', 'lightgrey')
+            .on('mouseover', overHandler)
+            .on('mouseout', outHandler);
         
+        var labels = canvas.append('g').selectAll('labels').data(jsonData.nodes)
+            .enter().append('text')
+            .attr('text-anchor', 'middle')
+            .attr('x', 100)
+            .attr('y', 100)
+            .text(function(data) { return data.name});
+
+        // definiera en kraft (gravity)
+        var simulation = d3.forceSimulation()
+            // manybody simulerar gravity (pull together) eller electrostatic charge (repulsion)
+            .force("charge", d3.forceManyBody().strength(-30) )
+            //centreringskraften skuffar alla noder mot mitten
+            .force("center", d3.forceCenter(gravityCenterX, gravityCenterY) )
+
+        // vi måste starta vår simulation och köra den on("tick")
+        simulation.nodes(jsonData.nodes).on("tick", tickHandler);
+
+        //vad ska göras varje tick när vi animerar?
+        function tickHandler() {
+            // x och y för ellipserna ska ändra i och med kraften
+            node
+                .attr('cx', function(data) { return data.x })
+                .attr('cy', function(data) { return data.y })
+
+            labels
+                .attr('x', function(data) { return data.x })
+                .attr('y', function(data) { return data.y + 5 })
+        };
+
+        function overHandler() {
+            d3.select(this).append("title").text(function(d) { return d.name })
+                .attr('class','ellipse-title');
+        };
+
+        function outHandler() {
+            d3.select('.ellipse-title').remove();
+        };
+
     });
 }
