5ed8a383feed89650500c8f537a8cfcf4d931884
hallchri <hallchri@arcada.fi>
2020-05-27 22:33:13 +0300

    Lite allt möjligt

--- boxPlot2.js
+++ boxPlot2.js
@@ -1,13 +1,40 @@
-lineIsDrawn = false;
-boxIsDrawn =  false;
+//window.onload = redraw();
+var cryptoData = [];
+function redraw() {
+    var currency = "";
+    var dateFrom = 0;
+    var dateTo = 0;
+    var dateButton = document.getElementById('submit-date');
+    
+
+    dateButton.addEventListener('click', function() {
+        currency = document.getElementById('currency-list');
+        currency = currency.value;
+        dateFrom = new Date(Date.parse(document.getElementById('start-date').value));
+        dateTo = new Date(Date.parse(document.getElementById('end-date').value));
+        var diff = (dateTo.getTime() - dateFrom.getTime()) / (1000 * 60 * 60 * 24);
+        //console.log(diff);
+
+        $.get("https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=" + currency + "&limit=" + diff + "&toTs=" + ((Date.parse(document.getElementById('end-date').value)) / 1000) + "",function(data, status) { 
+            d3.select('.canvas').remove();
+            console.log(data);
+            cryptoData = data.Data.Data;
+            var date = new Date(data.Data.Data[data.Data.Data.length - 1].time * 1000);
+            console.log("datum " + date);
+            console.log(lineIsDrawn);
+            console.log(boxIsDrawn);
+            redraw();
+        });
+    });
+
+    var lineIsDrawn = false;
+    var boxIsDrawn =  false;
 
 function drawChart() {
-    $.get("https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=10",function(data, status) {   
-
-        var cryptoData = data.Data.Data;
         var width = 800, height = 800, margin = 30;
         var chartWidth = width - (margin*2);
         var chartHeight = height - (margin*2);
+        var yMargin = 500;
         var boxWidth = 50;
         var boxX = chartWidth-boxWidth;
         var lineX = boxX + boxWidth / 2;
@@ -34,139 +61,125 @@ function drawChart() {
         console.log(median);
         console.log("lQ " + lQ);
         console.log("uQ " + uQ);
-        console.log("min " + minV);
-        console.log("max " + maxV);
+        console.log("min " + min);
+        console.log("max " + max);
         console.log("tid " + timeSamples);
         console.log("open " + cryptoValues);
 
         var minTime = d3.min(timeSamples);
         var maxTime = d3.max(timeSamples);
 
+        var format = d3.timeFormat("%x");
+        
         var xScale = d3.scaleLinear()
-            .domain([minTime - 100000, maxTime+100000])
+            .domain([minTime * 1000, maxTime * 1000])
             .range([0, chartWidth]);
 
-        var xAxis = d3.axisBottom(xScale);
+        var xAxis = d3.axisTop(xScale)
+            .ticks(5)
+            .tickFormat(format)
+            .tickPadding(20);
 
-        var minY = d3.min(minV);
-        var maxY = d3.max(maxV);
+        var minY = min;
+        var maxY = max;
         var yScale = d3.scaleLinear()
-            .domain([minY-1000, maxY])
+            .domain([minY - 1000, maxY + 1000])
             .range([chartHeight, 0]);
         
-        var yAxis = d3.axisRight(yScale).ticks(20);
+        var yAxis = d3.axisRight(yScale)
+        .ticks(medianV.length-1)
+        .tickPadding(20);
 
         var canvas = d3.select('body').append('svg')
+            .attr('class', 'canvas')
             .attr('width', width)
             .attr('height', height);
 
         var chartGroup = canvas.append('g')
-            .attr("transform","translate(0,0)");
+            .attr("transform","translate(50,50)");
 
-
-
-
-        function drawBoxPlot() {
-
-            if(lineIsDrawn) {
-                d3.select('g.line-group').remove();
-            }
-
-            if(!boxIsDrawn) {
-            var boxGroup = chartGroup.append('g')
-                .attr('class', 'box-group');
-
-
-                // rita en linje från min till maxvärdet
-            boxGroup.append('line')
-                .attr('width', 5)
-                .attr('stroke', 'black')
-                .attr('x1', chartWidth/2 + boxWidth / 2)
-                .attr('x2', chartWidth/2 + boxWidth / 2)
-                .attr('y1', yScale(min))
-                .attr('y2', yScale(max));
         
-            boxGroup.append('rect')
-                .attr('class', 'boxes')
-                .attr('width', boxWidth)
-                .attr('height', yScale(median))
-                .attr('fill','gray')
-                .attr('stroke', 'black')
-                .attr('x', chartWidth/2)
-                .attr('y', yScale(uQ));
-
-            // rita en linje vertikalt vid minsta värdet
-            boxGroup
-                .append('line')
-                .attr('class', 'line-vert-min')
-                .attr('width', 5)
-                .attr('stroke', 'black')
-                .attr('x1', chartWidth/2)
-                .attr('x2', chartWidth/2 + boxWidth )
-                .attr('y1', yScale(min))
-                .attr('y2', yScale(min));
-            
-            // rita en linje vertikalt vid medianen
-            boxGroup.append('line')
-                .attr('width', 5)
-                .attr('stroke', 'black')
-                .attr('x1', chartWidth/2)
-                .attr('x2', chartWidth / 2 + boxWidth)
-                .attr('y1', yScale(median))
-                .attr('y2', yScale(median));
-
-            // rita en linje vertikalt vid max värdet
-            boxGroup.append('line')
-                .attr('width', 5)
-                .attr('stroke', 'black')
-                .attr('x1', chartWidth/2)
-                .attr('x2', chartWidth/2 + boxWidth)
-                .attr('y1', yScale(max))
-                .attr('y2', yScale(max));
-
-                boxIsDrawn = true;
-                lineIsDrawn = false;
-            }
-
-        };
-
-
-        function drawLineChart() {
-            
-            if(boxIsDrawn) {
-                d3.select('g.box-group').remove();
-            }
-
-            if(!lineIsDrawn) {
-
-            var lineGroup = chartGroup.append('g')
-                .attr('class','line-group');
-
-            var path = d3.line()
-                .x( function(d,i) { return xScale(timeSamples[i]) } )
-                .y( function(d,i) { return yScale(cryptoValues[i]) } );
-
-            lineGroup.append('path')
-                .attr('fill','none')
-                .attr('stroke','blue')
-                .attr('d', path(cryptoData));
-
-                lineIsDrawn = true;
-            }
-
-            boxIsDrawn = false;
-
-        };
-
         // Rita ut axeln
         chartGroup.append('g').call(xAxis);
         chartGroup.append('g').call(yAxis);
 
-        var lineButton = document.getElementById('lineButton');
-        lineButton.addEventListener('click', drawLineChart);
+        var lineButton = document.getElementById('line-button');
+        lineButton.addEventListener('click', function() {
+            if(!lineIsDrawn) {
+                d3.select('.box-group').remove();
+                var lineGroup = chartGroup.append('g')
+                    .attr('class','line-group');
 
-        var boxButton = document.getElementById('boxButton');
-        boxButton.addEventListener('click', drawBoxPlot);
+                var path = d3.line()
+                    .x( function(d,i) { return xScale(timeSamples[i] * 1000) } )
+                    .y( function(d,i) { return yScale(cryptoValues[i]) } );
 
-    });    
-};
+                lineGroup.append('path')
+                    .attr('fill','none')
+                    .attr('stroke','blue')
+                    .attr('d', path(cryptoData));
+            }
+                lineIsDrawn = true;
+                boxIsDrawn = false;
+        });
+
+        var boxButton = document.getElementById('box-button');
+        boxButton.addEventListener('click', function() {
+            if(!boxIsDrawn) {
+                d3.select('.line-group').remove();
+                var boxGroup = chartGroup.append('g')
+                        .attr('class', 'box-group');
+
+                // rita en linje från min till maxvärdet
+                 boxGroup.append('line')
+                    .attr('width', 5)
+                    .attr('stroke', 'black')
+                    .attr('x1', chartWidth/2 + boxWidth / 2)
+                    .attr('x2', chartWidth/2 + boxWidth / 2)
+                    .attr('y1', yScale(min))
+                    .attr('y2', yScale(max));
+                
+                // rita själva rektangeln
+                boxGroup.append('rect')
+                    .attr('class', 'boxes')
+                    .attr('width', boxWidth)
+                    .attr('height', (yScale(lQ) - yScale(uQ)))
+                    .attr('fill','gray')
+                    .attr('stroke', 'black')
+                    .attr('x', chartWidth/2)
+                    .attr('y', yScale(uQ));
+
+                // rita en linje vertikalt vid minsta värdet
+                boxGroup.append('line')
+                    .attr('class', 'line-vert-min')
+                    .attr('width', 5)
+                    .attr('stroke', 'black')
+                    .attr('x1', chartWidth/2)
+                    .attr('x2', chartWidth/2 + boxWidth )
+                    .attr('y1', yScale(min))
+                    .attr('y2', yScale(min));
+                    
+                // rita en linje vid medianen
+                boxGroup.append('line')
+                    .attr('width', 5)
+                    .attr('stroke', 'black')
+                    .attr('x1', chartWidth/2)
+                    .attr('x2', chartWidth / 2 + boxWidth)
+                    .attr('y1', yScale(median))
+                    .attr('y2', yScale(median));
+
+                // rita en linje vid max värdet
+                boxGroup.append('line')
+                    .attr('width', 5)
+                    .attr('stroke', 'black')
+                    .attr('x1', chartWidth/2)
+                    .attr('x2', chartWidth/2 + boxWidth)
+                    .attr('y1', yScale(max))
+                    .attr('y2', yScale(max));
+            }
+                boxIsDrawn = true;
+                lineIsDrawn = false;
+            });
+        }
+        drawChart();
+    };
